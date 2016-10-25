e78e0aced207aa8ca3aba3a41f2502951bd98aa6
Filipe Cabaco <filipe@talkdesk.com>
2016-10-25 23:28:36 +0100

    Fix flag of hackney pool.
    Thanks to the input from benoitc the ssl session is not being re-used.
    
    This happened because we were using the same connection pool, so
    setting it to false was enough to fix the issue.

--- lib/cert.ex
+++ lib/cert.ex
@@ -5,13 +5,15 @@ defmodule Cert do
   def process_url(url), do: "#{@base_url}/api/#{url}"
 
   def call_api(certfile, keyfile) do
-    case Cert.get("test",
-	  [],
-	  ssl: [certfile: certfile,
-		keyfile: keyfile,
-		reuse_sessions: false],
-	  hackney: [pool: nil]) do
-      {:err, _} -> :err
+    res = Cert.get("test",
+      [],
+      ssl: [certfile: certfile,
+	    keyfile: keyfile,
+	    reuse_sessions: false],
+      hackney: [pool: false])
+
+    case res do
+      {:error, _} -> :err
       _ -> :ok
     end
   end
